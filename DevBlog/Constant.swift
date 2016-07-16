2ec00f3d43e16df912ae6fa08e22a269e4a32f12
ittus <vuminhthang.cm@gmail.com>
2016-07-16 18:47:17 +0800

    init project and design login screen

---
+++ DevBlog/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  DevBlog
+//
+//  Created by Minh Thang Vu on 7/16/16.
+//  Copyright © 2016 Minh Thang Vu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ DevBlog/MaterialView.swift
@@ -0,0 +1,20 @@
+//
+//  MaterialView.swift
+//  DevBlog
+//
+//  Created by Minh Thang Vu on 7/16/16.
+//  Copyright © 2016 Minh Thang Vu. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        
+    }
+}
