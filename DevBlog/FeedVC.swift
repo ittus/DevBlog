527327a260996b9bf2044b2fa2bc81b4e049190b
ittus <vuminhthang.cm@gmail.com>
2016-07-23 14:48:07 +0800

    upload image to imageshack

--- DevBlog/FeedVC.swift
+++ DevBlog/FeedVC.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 import Firebase
+import Alamofire
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var txtPOst: MaterialTextField!
+    
+    @IBOutlet weak var ivImageSelected: UIImageView!
+    
     var posts = [Post]()
     static var imageCache = NSCache()
+    
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +28,9 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         tableView.estimatedRowHeight = 393
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observeEventType(FIRDataEventType.Value, withBlock: { (snapshot) in
             print(snapshot.value)
@@ -76,4 +86,54 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             return tableView.estimatedRowHeight
         }
     }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        ivImageSelected.image = image
+    }
+    
+    @IBAction func selectImageTap(sender: UITapGestureRecognizer) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    @IBAction func postButtonPressed(sender: UIButton) {
+        if let txt = txtPOst.text where txt != "" {
+            if let img = ivImageSelected.image {
+                // TODO: Make sure image is not camera image
+                let urlStr = "https://post.imageshack.us/upload_api.php"
+                let url = NSURL(string: urlStr)!
+                let imgData = UIImageJPEGRepresentation(img, 0.2)!
+                let keyData = ""
+                .dataUsingEncoding(NSUTF8StringEncoding)!
+                
+                let keyJSON = "json".dataUsingEncoding(NSUTF8StringEncoding)!
+                
+                Alamofire.upload(.POST, url, multipartFormData: { (multipartFormData: MultipartFormData) in
+                    multipartFormData.appendBodyPart(data: imgData, name: "fileupload", fileName: "image", mimeType: "image/jpg")
+                    multipartFormData.appendBodyPart(data: keyData, name: "key")
+                    multipartFormData.appendBodyPart(data: keyJSON, name: "format")
+                    }, encodingCompletion: { (encodeResult: Manager.MultipartFormDataEncodingResult) in
+                        switch encodeResult {
+                        case .Success(let upload, _ , _):
+                            upload.responseJSON(completionHandler: { (response: Response<AnyObject, NSError>) in
+                                if let info = response.result.value as? Dictionary<String, AnyObject> {
+                                    if let links = info["links"] as? Dictionary<String, AnyObject> {
+                                        if let imgLink = links["image_link"] as? String {
+                                            print("Link \(imgLink)")
+                                        }
+                                    }
+                                }
+                            })
+                        case .Failure(let error):
+                            print(error)
+                        }
+                })
+                
+            }
+        }
+    }
+    
+    
+    
 }
